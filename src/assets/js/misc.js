f2b4d1e5b019ce61ed3e62ad9a4fb2a323a91e37
Damien Williams <12691904+kingdeejay@users.noreply.github.com>
2022-11-29 18:48:03 -0500

    Add files via upload

--- src/assets/js/misc.js
+++ src/assets/js/misc.js
@@ -1,1 +1,9 @@
+(function(){
+    //Add a body class once page has loaded
+    //Used to add CSS transition elements
+    //Avoids content shifting during page load
 
+    window.addEventListener('load', function(){
+     document.body.classList.add('page-loaded');
+    });
+})();
