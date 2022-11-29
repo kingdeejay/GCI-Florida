ac9080c761f40f39939393c465cd756ca2ed3f67
Damien Williams <12691904+kingdeejay@users.noreply.github.com>
2022-11-29 18:48:30 -0500

    Add files via upload

---
+++ src/assets/js/theme.js
@@ -0,0 +1,11 @@
+//Bootstrap
+import 'bootstrap';
+
+//Components 
+import './components/aos';
+import './components/swiper';
+import './components/nav';
+//import './components/multer'
+
+//theme misc
+import './misc';
